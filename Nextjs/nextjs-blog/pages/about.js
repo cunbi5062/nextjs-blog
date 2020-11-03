25ac0966febe82bc79d693e8905ac3976ac7bde5
Đỗ Việt Anh <63449089+cunbi5062@users.noreply.github.com>
2020-11-03 14:50:32 +0700

    delete

--- Nextjs/nextjs-blog/pages/about.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const About = () => (
-  <div>
-    <h1>This is your app's about page</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit porro deleniti amet. Error ipsam soluta quas maiores quibusdam accusamus dignissimos repellendus minus! Odio repudiandae ipsum impedit magnam odit non cum?</p>
-  </div>
-)
-export default About
